6d2fc01ff910b07b89821d7abce86fdffa30e760
szabomate100 <szabomate100@gmail.com>
2023-09-21 12:02:25 +0200

    copy files

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+int main()
+{
+    constexpr int N_ELEMENTS = 100;
+
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa\n";
+    for (int i = 0; i<=99; i++)
+    {
+        b[i] = (i+1) * 2;
+    }
+    for (int j = 0; j<=99; j++)
+    {
+        std::cout << "Ertek:" << b[j] << ' ';
+    }    
+    std::cout << "\nAtlag szamitasa: " << std::endl;
+    int atlag;
+    for (int k = 0; k < N_ELEMENTS; k++)
+    {
+        atlag += b[k];
+    }
+    atlag /= N_ELEMENTS;
+    std::cout << "Atlag: " << atlag << std::endl;
+    return 0;
+}
